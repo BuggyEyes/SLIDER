8b3986c40e75c886abb5fe9f9c351ac3ff5eaa7a
Buggyeyes <ovel2405@yandex.ru>
2023-06-12 20:29:08 +0300

    123

--- scripts.js
+++ scripts.js
@@ -1,26 +1,46 @@
 // Массив картинок
 const entities = [
     {
-        text: 'Первый кот',
-        img: 'https://s1.stc.all.kpcdn.net/putevoditel/projectid_103889/images/tild3463-3139-4630-b563-646166616434__20180225_gaf_uw8_101.jpg'
+        city: 'Rostov-on-Don\nLCD admiral',
+        apart_area: '81 m2',
+        repair_time: '3.5 months',
+        repair_cost: 'Upon request',
+        img: 'SVG/image1.svg',
     },
     {
-        text: 'Второй кот',
-        img: 'https://avatars.mds.yandex.net/i?id=6bc83af104f35acde0c9bf41fe2b46258b5e15a7-8497474-images-thumbs&n=13'
+        city: 'Sochi Thieves',
+        apart_area: '105 m2',
+        repair_time: '4 months',
+        repair_cost: 'Upon request',
+        img: 'SVG/image2.svg',
+
     },
     {
-        text: 'Третий кот',
-        img: 'https://images.unsplash.com/photo-1591871937573-74dbba515c4c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80'
+        city: 'Rostov-on-Don Patriotic',
+        apart_area: '93 m2',
+        repair_time: '3 months',
+        repair_cost: 'Upon request',
+        img: 'SVG/image3.svg',
+
     }
 ]
 
 // Добавляем ивенты текста и картинки
-const text = document.querySelector('.text')
+const city = document.querySelector('.city')
+const apart_area = document.querySelector('.apart_area')
+const repair_time = document.querySelector('.repair_time')
+const repair_cost = document.querySelector('.repair_cost')
 const img = document.querySelector('.image')
-
+// const rostov_adm = document.querySelector('rostov_adm')
+// const rostov_pat = document.querySelector('rostov_pat')
+// const sochi = document.querySelector('sochi')
 const setEntity = (index) => {
-    text.innerText = entities[index].text
+    city.innerText = entities[index].city
+    apart_area.innerText = entities[index].apart_area
+    repair_time.innerText = entities[index].repair_time
+    repair_cost.innerText = entities[index].repair_cost
     img.style.backgroundImage = `url(${entities[index].img})`
+
 }
 
 const prev = document.querySelector('.prev')
@@ -33,10 +53,9 @@ prev.addEventListener('click', () => {
         setEntity(currentIndex - 1);
         currentIndex--
     } else {
-        currentIndex = entities.length-1;
+        currentIndex = entities.length - 1;
         setEntity(entities.length - 1);
     }
-
 
 
 })
@@ -50,22 +69,47 @@ next.addEventListener('click', () => {
     }
 
 
-
 })
 
 const dot_1 = document.querySelector('.dot_1');
 const dot_2 = document.querySelector('.dot_2');
 const dot_3 = document.querySelector('.dot_3');
 
-dot_1.addEventListener('click', () =>{
+dot_1.addEventListener('click', () => {
     setEntity(0);
-   // currentIndex = 0;
+    // currentIndex = 0;
 })
-dot_2.addEventListener('click', () =>{
-setEntity(1);
-   // currentIndex = 1;
+dot_2.addEventListener('click', () => {
+    setEntity(1);
+    // currentIndex = 1;
 })
-dot_3.addEventListener('click', () =>{
-setEntity(2);
-   // currentIndex = 2;
+dot_3.addEventListener('click', () => {
+    setEntity(2);
+    // currentIndex = 2;
+})
+// Навигация над изображением
+const rostov_adm = document.querySelector('.rostov_adm');
+const sochi = document.querySelector('.sochi');
+const rostov_pat = document.querySelector('.rostov_pat');
+
+rostov_adm.addEventListener('click', () => {
+    setEntity(0);
+    rostov_adm.style.cssText = 'color: #E3B873; text-decoration: underline; opasity: 0%';
+    rostov_pat.style.cssText = 'color: white; text-decoration: none';
+    sochi.style.cssText = 'color: white; text-decoration: none';
+    // currentIndex = 0;
+})
+sochi.addEventListener('click', () => {
+    setEntity(1);
+    rostov_adm.style.cssText = 'color: white; text-decoration: none';
+    rostov_pat.style.cssText = 'color: white; text-decoration: none';
+    sochi.style.cssText = 'color: #E3B873; text-decoration: underline; opasity: 0%';
+    // currentIndex = 1;
+})
+rostov_pat.addEventListener('click', () => {
+    setEntity(2);
+    rostov_pat.style.cssText = 'color: #E3B873; text-decoration: underline; opasity: 0%';
+    rostov_adm.style.cssText = 'color: white; text-decoration: none';
+    sochi.style.cssText = 'color: white; text-decoration: none';
+    // currentIndex = 2;
 })
